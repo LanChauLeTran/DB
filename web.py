f74a8bd851ddb9749becb9399e95a1213a8c69b7
Alan <atwpb@mst.edu>
2018-04-25 21:05:43 -0500

    Made it so user can upload files into a downloaded folder

--- web.py
+++ web.py
@@ -1,14 +1,17 @@
 import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from werkzeug.utils import secure_filename
 
-
+UPLOAD_FOLDER ='downloads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask('DataBase')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def home():
     return render_template('home.html')
-    
+
 @app.route('/student')
 def student():
     return render_template('student.html')
@@ -17,8 +20,16 @@ def student():
 def studentinfo():
     name = request.form['Name']
     id = request.form['ID']
-
     print(name + id)
     return render_template('studentinfo.html', name=name)
 
+@app.route('/upload', methods = ['POST', 'GET'])
+def upload():
+    file = request.files['inputFile']
+    if file.filename == '':
+            flash('No selected file')
+    else:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return file.filename
 app.run()
