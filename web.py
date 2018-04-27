8a2184c826a1605b736cd9d718271238cba550ef
Alan <atwpb@mst.edu>
2018-04-27 00:05:08 -0500

    Made it so exam saves as key and User is now allow to submit information along with uploading their exam

--- web.py
+++ web.py
@@ -1,10 +1,12 @@
 import os
 import sqlite3
+import random
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from werkzeug.utils import secure_filename
+from os import path
 
 UPLOAD_FOLDER ='downloads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'docx'])
 app = Flask('DataBase')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -23,13 +25,27 @@ def studentinfo():
     print(name + id)
     return render_template('studentinfo.html', name=name)
 
+
 @app.route('/upload', methods = ['POST', 'GET'])
 def upload():
     file = request.files['inputFile']
-    if file.filename == '':
-            flash('No selected file')
+    Coursenum = request.form['CourseNum']
+    Subject = request.form['Subject']
+    Exam_title = request.form['ExamTitle']
+    Semester = request.form['Semester']
+    if (Coursenum == '' or Subject == '' or Semester == ''):
+        flash('Course Number, Subject, or Semester can not be blank')
+    elif (file.filename == ''):
+        flash('No selected file')
     else:
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return file.filename
+        if(filename[-4] == "."):
+            extension = filename[-4:]
+        else: 
+            extension = filename[-5:]
+        filename = str(random.randint(1,999999999)) + extension
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) 
+        #cursor.execute('INSERT INTO Exams VALUES (Filename, Coursenum, Subject, Exam_title, Semester)')
+        #connection.commit()
+    return render_template('studentinfo.html')
 app.run()
